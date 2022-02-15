bab3257c15bcb9dd7bd700f1acb953881c9f4f10
J <wkdwogns2345@naver.com>
2022-02-15 20:12:40 +0900

    main room view togle finish

--- js/main.js
+++ js/main.js
@@ -13,3 +13,129 @@ $(document).ready(function(){
   }); 
   $.fn.fullpage.setAllowScrolling(ture);
 });
+$(function(){
+  tab();
+});
+
+function tab(){
+  $("#deluxe_ondol").click(function(){
+      $(".room_1").css("display","block");
+      $(".room_2").css("display","none");
+      $(".room_3").css("display","none");
+      $(".room_4").css("display","none");
+
+      $("#deluxe_ondol").css({
+        "font-weight":"900",
+        "color":"#2A286E"
+      });
+
+      $("#double").css({
+        "font-weight":"inherit",
+        "color":"black"
+      });
+      $("#twin").css({
+        "font-weight":"inherit",
+        "color":"black"
+      });
+      $("#suite").css({
+        "font-weight":"inherit",
+        "color":"black"
+      });
+
+
+      $(this).addClass("active");
+      $("#double").removeClass("active");
+      $("#twin").removeClass("active");
+      $("#suite").removeClass("active");
+  })
+  $("#double").click(function(){
+    $(".room_1").css("display","none");
+    $(".room_2").css("display","block");
+    $(".room_3").css("display","none");
+    $(".room_4").css("display","none");
+
+
+    $("#deluxe_ondol").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+
+    $("#double").css({
+      "font-weight":"900",
+      "color":"#2A286E"
+    });
+    $("#twin").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+    $("#suite").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+
+
+      $(this).addClass("active");
+      $("#deluxe_ondol").removeClass("active");
+      $("#twin").removeClass("active");
+      $("#suite").removeClass("active");
+  })
+  $("#twin").click(function(){
+    $(".room_1").css("display","none");
+    $(".room_2").css("display","none");
+    $(".room_3").css("display","block");
+    $(".room_4").css("display","none");
+
+    $("#deluxe_ondol").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+    $("#double").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+    $("#twin").css({
+
+      "font-weight":"900",
+      "color":"#2A286E"
+    });
+    $("#suite").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+
+
+      $(this).addClass("active");
+      $("#deluxe_ondol").removeClass("active");
+      $("#double").removeClass("active");
+      $("#suite").removeClass("active");
+  })
+  $("#suite").click(function(){
+    $(".room_1").css("display","none");
+    $(".room_2").css("display","none");
+    $(".room_3").css("display","none");
+    $(".room_4").css("display","block");
+
+    $("#deluxe_ondol").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+    $("#double").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+    $("#twin").css({
+      "font-weight":"inherit",
+      "color":"black"
+    });
+    $("#suite").css({
+      "font-weight":"900",
+      "color":"#2A286E"
+    });
+
+
+      $(this).addClass("active");
+      $("#deluxe_ondol").removeClass("active");
+      $("#double").removeClass("active");
+      $("#twin").removeClass("active");
+  })
+}
